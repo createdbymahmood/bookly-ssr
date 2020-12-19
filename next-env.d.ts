ade456b35e0229e6549fd65b62f499aee4dc5a8c
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-19 11:51:18 +0330

    chore: add process.env global types

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,16 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production';
+            API_URL: string;
+            PORT?: string;
+        }
+    }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
