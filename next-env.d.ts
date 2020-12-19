adaea09c0aac3f0590871ffe124adc02237cf5ec
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-19 12:33:20 +0330

    chore(types): :label: add some global types

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+declare module '*.svg';
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
@@ -10,7 +11,17 @@ declare global {
         }
     }
 }
+/* customize the AxiosError object type */
+declare module 'axios' {
+    interface ApiServiceError extends AxiosError<ServerError> {}
+}
 
+/* picks all types of a specific component */
+declare type $ElementProps<T> = T extends React.ComponentType<infer Props>
+    ? Props extends object
+        ? Props
+        : never
+    : never;
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
 export {};
