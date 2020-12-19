fa07e9894622e13baad37ef8b48722074070b0dc
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-19 16:07:44 +0330

    fix: fix api url again :/

--- src/helpers/generateFakeImageUrl.ts
+++ src/helpers/generateFakeImageUrl.ts
@@ -1,9 +1,10 @@
 import { FAKE_IMAGE_API_CONFIG } from 'constants/config';
 const { size, apiUrl, key } = FAKE_IMAGE_API_CONFIG;
+import { uniqueId } from './uniqueId';
 
 export const generateFakeImageUrl = (
     keyword: string = key,
     imageSize: string = size
 ): string => {
-    return `${apiUrl}/${size}`;
+    return `${apiUrl}/${size}?random=${uniqueId()}`;
 };
