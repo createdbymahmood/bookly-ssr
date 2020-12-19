0a5a969f4c04cee0e6546780cf9430920cfb6210
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-19 19:38:33 +0330

    refactor: read head main  title from single source

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,6 +1,8 @@
 import { default as NextHead } from 'next/head';
-
+/* types */
 import { FC, ReactNode } from 'react';
+/* constants */
+import { APP_TITLE } from 'constants/config';
 
 type Props = {
     children: ReactNode;
@@ -9,7 +11,9 @@ type Props = {
 export const Head: FC<Props> = props => {
     return (
         <NextHead>
-            <title> {props.title || 'bookly'} </title>
+            <title>
+                {props.title} | {APP_TITLE}
+            </title>
             {props.children}
         </NextHead>
     );
