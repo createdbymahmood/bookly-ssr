0157631a47438e5cc4f2a781db88ebbb5580ccfc
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-24 11:18:18 +0330

    fix: :bug:

--- src/components/hoc/injectAuthConfig.tsx
+++ src/components/hoc/injectAuthConfig.tsx
@@ -3,12 +3,12 @@ import { useRouter } from 'next/router';
 import { useIsomorphicLayoutEffect } from 'react-use';
 import { permissions, Permission } from 'services/rbac';
 import { Path } from 'types/global';
-import get from 'lodash.get';
+import get from 'lodash/get';
 
 export function injectAuthConfig(accessKey: Path<Permission>) {
     return function <T>(Component: ComponentType<T>) {
         return function (props: T): JSX.Element {
-            const hasAccess = get(permissions.guest, accessKey);
+            const hasAccess = get(permissions.guest, accessKey, undefined);
             if (hasAccess === undefined) {
                 throw new Error(`Permission key didn't correctly provided`);
             }
