bd0959157c6f599b07b9692cfd02ab0d78753e04
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-24 11:26:34 +0330

    fix: :bug:

--- src/pages/publishers/[publisherId].tsx
+++ src/pages/publishers/[publisherId].tsx
@@ -32,7 +32,7 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async ({
     return { props: { initialPublishers, publisherId } };
 };
 
-const Publisher: NextPage<PageProps> = ({ publisherId }) => {
+const PublisherPage: NextPage<PageProps> = ({ publisherId }) => {
     const { data } = useSinglePublisher(publisherId);
 
     const description = `لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.`;
@@ -64,4 +64,4 @@ const Publisher: NextPage<PageProps> = ({ publisherId }) => {
     );
 };
 
-export default injectLayoutConfig('publisher')(Publisher);
+export default injectLayoutConfig('publisher')(PublisherPage);
