2abfb09655ed804143c7836552d52259f618bb86
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-25 18:53:15 +0330

    feat: add setupWrapper func

---
+++ src/helpers/setupWrapper.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react';
+/* modules */
+import { render } from '@testing-library/react';
+/* services */
+import { RecoilService } from 'services/recoil/recoilService';
+import { ReactQueryService } from 'services/react-query/reactQueryService';
+import { ConfigProvider } from 'antd';
+
+/**
+ *
+ * @param Component component that we want to test
+ * @param props props of component that we want to test
+ * @description setups the wrappers to prepare for testing
+ */
+export const setupWrapper = <BaseProps extends {}>(
+    Component: FunctionComponent<BaseProps>,
+    props: BaseProps
+) =>
+    render(
+        <RecoilService>
+            <ReactQueryService>
+                <ConfigProvider direction="rtl">
+                    <Component {...(props as BaseProps)} />
+                </ConfigProvider>
+            </ReactQueryService>
+        </RecoilService>
+    );
